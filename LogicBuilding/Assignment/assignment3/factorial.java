b21e882df8ca07ff7638a2a342b618a344d5b1d4
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-22 16:03:06 +0530

    Create factorial.java

---
+++ LogicBuilding/Assignment/assignment3/factorial.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int n = 10;
+       int fact=1;
+       for(int i =1;i<=n;i++){
+           fact*=i;
+       }
+        System.out.println(fact);
+    }
+} //3628800 is the output
