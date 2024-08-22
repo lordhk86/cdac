48cf87197bc35cd47996c2ca534013cc885eed52
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-23 02:56:03 +0530

    Create 26.java

---
+++ LogicBuilding/Assignment/assignment3/26.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      int count = 5;
+      int n=1;
+    for (int i = 0; i <=count; i++) {
+        for(int j=1;j<=i;j++){
+            System.out.print(n++);
+        }
+           
+             System.out.println();
+        }
+          
+        }
+}
