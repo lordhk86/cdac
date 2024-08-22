87d3cfdad205a54c027504f147f46b618152a093
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-23 01:27:52 +0530

    Create eveN_50.java

---
+++ LogicBuilding/Assignment/assignment3/eveN_50.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+     int a =50;
+     for(int i =1;i<=a;i++){
+         if(i%2==0){
+              System.out.println(i);
+         }
+     }
+    
+    
+    }
+}
