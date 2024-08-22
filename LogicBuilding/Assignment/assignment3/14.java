4db347ddf9e00f9c0c32471b34dfaab39a0199f4
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-23 02:37:25 +0530

    Create 14.java

---
+++ LogicBuilding/Assignment/assignment3/14.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      int count = 9;
+    for (int i = 1; i <= count; i++) {
+        if(i%2!=0 || i==2){
+           for(int j=1;j<=i;j++){
+           System.out.print("*");
+           }
+             System.out.println();
+        }
+          
+        }
+   
+}
+}
