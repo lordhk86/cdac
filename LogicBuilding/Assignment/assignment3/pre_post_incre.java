e16457ced3b3364149bee73ab1f5b3bfac27d559
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-23 01:31:38 +0530

    Create pre_post_incre.java

---
+++ LogicBuilding/Assignment/assignment3/pre_post_incre.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+     int a =50;
+     int b=60;
+     int result = a++ + ++b + a-- + --b;
+     System.out.println(a);
+     System.out.println(b);
+    
+    System.out.println(result);
+    }
+}
