59547bbb9076c01f7c99d5635732a1792903a069
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-08-23 01:42:16 +0530

    Create 12.java

---
+++ LogicBuilding/Assignment/assignment3/12.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      int count = 5;
+      for(int i =0;i<count;i++){
+          for(int j=0;j<count;j++){
+              System.out.print("*");
+          }
+          System.out.println("");
+      }
+}
+}
