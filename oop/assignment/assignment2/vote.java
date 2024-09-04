70f979414db7662e803dcdd61d3ccb7f50977210
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-04 13:10:12 +0530

    Create vote.java

---
+++ oop/assignment/assignment2/vote.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+       int age=19;
+       if(age>=18)
+       System.out.println("you can vote");
+       else
+       System.out.println("you cannot vote");
+    }
+}
