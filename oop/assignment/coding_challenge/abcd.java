6a7d36b2d83436c581a6f77338dc2b4f7c15b7bf
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-04 17:27:11 +0530

    Create abcd.java

---
+++ oop/assignment/coding_challenge/abcd.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        char a='A';
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6-i;j++){
+                System.out.print(" ");
+            }
+            for(int z =0;z<i*2-1;z++){
+            System.out.print(a);
+            a++;
+            }
+        System.out.println("");     
+        }
+       
+    }
+}
