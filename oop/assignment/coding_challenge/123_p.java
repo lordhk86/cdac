9a178399f2329c3b3ea1bf622a53e4787c80d02b
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-04 17:41:01 +0530

    Create 123_p.java

---
+++ oop/assignment/coding_challenge/123_p.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        for(int i=1;i<6;i++){
+            for(int z =1;z<=i;z++){
+            if(z==1||z==i||i==5)
+            System.out.print(z+" ");
+            else
+            System.out.print("  ");
+            }
+        System.out.println("");     
+        }
+       
+    }
+}
