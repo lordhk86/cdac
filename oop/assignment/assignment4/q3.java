f96c164e1686497020a8399070029225cca6a41d
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-10 17:38:20 +0530

    Create q3.java

---
+++ oop/assignment/assignment4/q3.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        double d=10.23;
+        int i=23;float f=12.3f;
+        System.out.println(i+d+f);
+    }
+}//it converets int,float to doble dose automatic widening
