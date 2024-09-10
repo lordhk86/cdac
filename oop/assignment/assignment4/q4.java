7c37dffa8b5dcbcc0285a8d1a39edcd137dd4b3f
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-10 17:42:25 +0530

    Create q4.java

---
+++ oop/assignment/assignment4/q4.java
@@ -0,0 +1,11 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+      int i=12;
+      byte b=1;i=b;
+      double d=i;
+      float f=i;
+      String s=Integer.toString(i);
+        System.out.println(i+" "+d+" "+f+" "+s);
+    }
+}//cannot converet to boolean 
