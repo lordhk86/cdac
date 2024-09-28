5c5e07772d7f3243733e304d1b96f9eae14f642f
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-09-28 09:14:52 +0530

    Create 3.java

---
+++ ADS/Assignment/Assignment2/3.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+         String s="Hello";
+         String s1="";
+         for(int i=s.length()-1;i>=0;i--){
+             s1=s1+s.charAt(i);
+         }
+         System.out.println(s1);
+    }
+}
