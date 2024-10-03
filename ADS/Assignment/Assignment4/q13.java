9f7fed8a5a037df25876702eb2c8803b77284df5
ayushSingh <55738011+lordhk86@users.noreply.github.com>
2024-10-03 08:11:02 +0530

    Create q13.java

---
+++ ADS/Assignment/Assignment4/q13.java
@@ -0,0 +1,103 @@
+class Node{
+    int data;Node next;
+    Node(int data){
+        this.data=data;
+    }
+}
+class LinkedList{
+    Node head=null;
+    public void insert(int data){
+        Node n=new Node(data);
+        if(head ==null||data<head.data){
+            if(head==null){
+            head=n;
+            n.next=head;}else{
+                Node temp=head;
+                n.next=head;
+                  while (temp.next != head) {
+                temp = temp.next;
+                }
+
+                head.next=n; head=n;
+            }
+        }
+        else{
+            Node temp=head;Node p=null;
+            while(temp.next!=head&&temp.next.data<data){
+                temp=temp.next;
+            }
+            n.next=temp.next;
+            temp.next=n;
+           
+    }
+    }    
+    public void del(int data){
+        if(head==null) return;
+        else{
+            Node temp=head; Node p=null;
+            if(temp.data == data) {
+                head=head.next;
+            }
+            while(temp!=null&&temp.data!=data){
+               p =temp;
+                temp=temp.next;
+            } 
+            if(temp==null) return ;
+            p.next=temp.next;
+        }
+    }
+    public boolean search(int data){
+        if(head == null ) return false;
+        else{
+            Node temp=head;
+            while(temp!=null){
+                if(temp.data==data) return true;
+                temp=temp.next;
+            }
+            return false;
+        }
+    }
+    public boolean cir(){
+        if(head == null ) return false;
+        else{
+            Node temp=head;Node p=head;
+            while(p!=null&&p.next!=null){
+                temp=temp.next;p=p.next.next;
+                if(temp==p){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    public void reverse(){
+        if(head == null) return ;
+        else{
+            Node temp=head;Node p = null ; Node r=null;
+            while(temp!=null){
+                r=temp.next;
+                temp.next=p;
+                p=temp;
+                temp=r;
+            }
+            head=p;
+        }
+    }
+    public void display(){
+        Node temp=head;
+        while(temp.next!=head){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+         System.out.print(temp.data+"->");
+        System.out.println("");
+    }
+}
+public class HelloWorld {
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        l.insert(10);l.insert(20);l.insert(30);l.insert(40);l.insert(25);
+        l.display();
+       
+    }
+}
